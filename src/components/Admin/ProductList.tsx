d698814a7df7219541dc08f18cf2c95828a8089c
DESKTOP-VDMI65C\PC <contactwork.huytc@gmail.com>
2025-03-18 10:06:05 +0700

    AdminPage_ProductList_TempData

--- src/components/Admin/ProductList.tsx
+++ src/components/Admin/ProductList.tsx
@@ -4,6 +4,12 @@ const cx = classNames.bind(styles);
 
 import { Link } from "react-router-dom";
 
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+
 function ProductList() {
   return (
     <div className={cx("frame")}>
@@ -16,16 +22,20 @@ function ProductList() {
             <th>Description</th>
             <th>Rate</th>
             <th>Character</th>
-            <th>Type</th>
-            <th>Classify</th>
+            <th>Prod type</th>
+            {/* Tags === Classify */}
+            <th>Tags</th>
+            {/* Tối thiếu 3 tấm  */}
             <th>Images</th>
             <th>Demo</th>
             <th>Quantity</th>
             <th>Sale Off</th>
+            <th>EDIT</th>
+            <th>DEL</th>
           </tr>
         </thead>
         <tbody>
-          {/* lam_dev map dữ liệu động ở đây */}
+          {/* lam_dev map dữ liệu động ở đây, chỗ "..." sẽ hiện các trường con */}
           <tr>
             <td>1</td>
             <td>Jagged Honor</td>
@@ -41,11 +51,38 @@ function ProductList() {
             </td>
             <td>0</td>
             <td>0</td>
+            <td>
+              <EditIcon className={cx("func")} />
+            </td>
+            <td>
+              <DeleteIcon className={cx("func")} />
+            </td>
           </tr>
         </tbody>
       </table>
 
-      <div className={cx("pagination")}></div>
+      <div className={cx("create-product")}>
+        <div className={cx("create-box")}>
+          <AddCircleIcon className={cx("create")} />
+          <h2 className={cx("text")}>Create Product</h2>
+        </div>
+      </div>
+
+      {/* Phân trang */}
+      <div className={cx("pagination")}>
+        <div className={cx("pagination-box")}>
+          <ArrowLeftIcon className={cx("page-change")} />
+          {/* lam_dev map dữ liệu động từ api */}
+          <div className={cx("page-numbers")}>
+            {["1", "2", "3", "4", "5", "..."].map((num, index) => (
+              <span className={cx("numbers")} key={index}>
+                {num}
+              </span>
+            ))}
+          </div>
+          <ArrowRightIcon className={cx("page-change")} />
+        </div>
+      </div>
     </div>
   );
 }
