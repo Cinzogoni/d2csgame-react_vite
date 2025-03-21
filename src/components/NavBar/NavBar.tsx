03cfc1686aae83912d857fa947d0ca2b3e4cf1e1
vtuyenlam1998 <vtuyenlam1998@gmail.com>
2025-03-22 00:11:18 +0700

    fix minor navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -18,6 +18,8 @@ const handleMenuLink = (classify: string, productType: string) => {
     return `/steam/${classify}`;
   } else if (productType === "STEAM_POINT") {
     return `/steam/${classify}`;
+  } else if (productType === "STEAM_LEVEL") {
+    return `/steam/${classify}`;
   } else if (productType === "WEATHER_EFFECTS") {
     return `/others/${classify}`;
   } else if (productType === "TERRAIN") {
@@ -44,6 +46,8 @@ const handleSubMenuLink = (
     return `/steam/${classify}/${isClass}`;
   } else if (productType === "STEAM_POINT") {
     return `/steam/${classify}/${isClass}`;
+  } else if (productType === "STEAM_LEVEL") {
+    return `/steam/${classify}/${isClass}`;
   } else if (productType === "WEATHER_EFFECT") {
     return `/others/${classify}/${isClass}`;
   } else if (productType === "TERRAIN") {
@@ -60,6 +64,7 @@ const handleSubMenuLink = (
 type SubMenuType = {
   id: number;
   title: string;
+  slug: string;
   link: typeof handleSubMenuLink;
   productType: string;
 };
@@ -67,6 +72,7 @@ type SubMenuType = {
 type MenuType = {
   id: number;
   title: string;
+  slug: string;
   link: typeof handleMenuLink;
   productType: string;
   subMenu: SubMenuType[] | null;
@@ -95,29 +101,34 @@ function NavBar() {
           id: 1,
           title: "Collector's cache",
           productType: "SET",
+          slug: "collector-cache",
           link: handleMenuLink,
           subMenu: [
             {
               id: 1,
-              title: "The International 09",
+              title: "The International 2019 Collector's Cache",
+              slug: "the-international-2019-collector-cache",
               productType: "SET",
               link: handleSubMenuLink,
             },
             {
               id: 2,
-              title: "The International 10",
+              title: "The International 2020 Collector's Cache",
+              slug: "the-international-2020-collector-cache",
               productType: "SET",
               link: handleSubMenuLink,
             },
             {
               id: 3,
-              title: "Aghanim Cache",
+              title: "Aghanim's 2021 Collector's Cache",
+              slug: "aghanim-2021-collector-cache",
               productType: "SET",
               link: handleSubMenuLink,
             },
             {
               id: 4,
-              title: "Diretide 2022",
+              title: "Diretide 2022 Collector's Cache",
+              slug: "diretire-2022-collector-cache",
               productType: "SET",
               link: handleSubMenuLink,
             },
@@ -125,7 +136,8 @@ function NavBar() {
         },
         {
           id: 2,
-          title: "Mix set",
+          title: "Mix Set",
+          slug: "mix-set",
           productType: "SET",
           link: handleMenuLink,
           subMenu: null,
@@ -133,6 +145,7 @@ function NavBar() {
         {
           id: 3,
           title: "Infused",
+          slug: "infused",
           productType: "SET",
           link: handleMenuLink,
           subMenu: null,
@@ -140,6 +153,7 @@ function NavBar() {
         {
           id: 4,
           title: "Arcana",
+          slug: "arcana",
           productType: "SET",
           link: handleMenuLink,
           subMenu: null,
@@ -154,42 +168,49 @@ function NavBar() {
         {
           id: 1,
           title: "Immortal",
+          slug: "immortal",
           productType: "ITEM",
           link: handleMenuLink,
           subMenu: [
             {
               id: 1,
-              title: "The International 3",
+              title: "Immortal Treasure 2019",
+              slug: "immortal-treasure-2019",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
             {
               id: 2,
-              title: "The International 4",
+              title: "Immortal Treasure 2020",
+              slug: "immortal-treasure-2020",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
             {
               id: 3,
-              title: "The International 5",
+              title: "Nemestice 2021 Immortal Treasure",
+              slug: "nemestice-2021-immortal-treasure",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
             {
               id: 4,
-              title: "Trove 2019",
+              title: "Trove Carafe 2019",
+              slug: "trove-carafe-2019",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
             {
               id: 5,
-              title: "Diretide 2020",
+              title: "Crimson Immortal",
+              slug: "crimson-immortal",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
             {
               id: 6,
-              title: "Golden",
+              title: "Golden Immortal",
+              slug: "golden-immortal",
               productType: "ITEM",
               link: handleSubMenuLink,
             },
@@ -198,6 +219,7 @@ function NavBar() {
         {
           id: 2,
           title: "Arcana",
+          slug: "arcana",
           productType: "ITEM",
           link: handleMenuLink,
           subMenu: null,
@@ -205,6 +227,7 @@ function NavBar() {
         {
           id: 3,
           title: "Dota Plus",
+          slug: "dota-plus",
           productType: "ITEM",
           link: handleMenuLink,
           subMenu: null,
@@ -219,6 +242,7 @@ function NavBar() {
         {
           id: 1,
           title: "Steam Wallet",
+          slug: "steam-wallet",
           productType: "STEAM_WALLET",
           link: handleMenuLink,
           subMenu: null,
@@ -226,7 +250,16 @@ function NavBar() {
         {
           id: 2,
           title: "Steam Point",
+          slug: "steam-point",
           productType: "STEAM_POINT",
+          link: handleMenuLink,
+          subMenu: null,
+        },
+        {
+          id: 3,
+          title: "Steam Level",
+          slug: "steam-level",
+          productType: "STEAM_LEVEL",
           link: handleMenuLink,
           subMenu: null,
         },
@@ -240,6 +273,7 @@ function NavBar() {
         {
           id: 1,
           title: t("Heros.weatherEffects"),
+          slug: "weather-effect",
           productType: "WEATHER_EFFECTS",
           link: handleMenuLink,
           subMenu: null,
@@ -247,6 +281,7 @@ function NavBar() {
         {
           id: 2,
           title: t("Heros.terrain"),
+          slug: "terrain",
           productType: "TERRAIN",
           link: handleMenuLink,
           subMenu: null,
@@ -254,6 +289,7 @@ function NavBar() {
         {
           id: 3,
           title: t("Heros.musicPack"),
+          slug: "music-pack",
           productType: "MUSIC_PACK",
           link: handleMenuLink,
           subMenu: null,
@@ -261,9 +297,25 @@ function NavBar() {
         {
           id: 4,
           title: t("Heros.courier"),
+          slug: "courier",
           productType: "COURIER",
           link: handleMenuLink,
-          subMenu: null,
+          subMenu: [
+            {
+              id: 1,
+              title: "Unusual Courier",
+              slug: "unusual-courier",
+              productType: "COURIER",
+              link: handleSubMenuLink,
+            },
+            {
+              id: 2,
+              title: "Unlocked Style Courier",
+              slug: "unlocked-style-courier",
+              productType: "COURIER",
+              link: handleSubMenuLink,
+            },
+          ],
         },
       ],
     },
@@ -293,13 +345,13 @@ function NavBar() {
                     {nav.menu.map((menu) => (
                       <Fragment key={menu.id}>
                         <Link
-                          to={menu.link(menu.title, menu.productType) ?? "/"}
+                          to={menu.link(menu.slug, menu.productType) ?? "/"}
                           className={cx("link")}
                           onMouseEnter={() => setHoveredSubMenuId(menu.id)}
                           onMouseLeave={() => setHoveredSubMenuId(null)}
                         >
                           <h4 className={cx("menu-title")}>{menu.title}</h4>
-                          {["Collector's cache", "Immortal"].includes(
+                          {["Collector's cache", "Immortal", "Courier"].includes(
                             menu.title
                           ) && (
                             <KeyboardDoubleArrowRightIcon
@@ -320,8 +372,8 @@ function NavBar() {
                                       <Link
                                         to={
                                           subMenu.link(
-                                            menu.title,
-                                            subMenu.title,
+                                            menu.slug,
+                                            subMenu.slug,
                                             subMenu.productType
                                           ) ?? "/"
                                         }
